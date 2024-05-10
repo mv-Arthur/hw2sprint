fb4ffa1f29a50a58b4e808b1f1242adbf7f8ed52
malakhovarthur <malakhov051@gmail.com>
2024-05-10 15:45:44 +0300

    hw 8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,44 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: "sort"; payload: "up" | "down" } | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+	// need to fix any
+	switch (action.type) {
+		case "sort": {
+			// by name
+			return action.payload === "up"
+				? [
+						...state.sort((a: UserType, b: UserType) => {
+							if (a.name.toLowerCase() < b.name.toLowerCase()) {
+								return -1;
+							}
 
-            return state // need to fix
-        }
-        case 'check': {
+							if (a.name.toLowerCase() > b.name.toLowerCase()) {
+								return 1;
+							}
 
-            return state // need to fix
-        }
-        default:
-            return state
-    }
-}
+							return 0;
+						}),
+				  ]
+				: [
+						...state.sort((a: UserType, b: UserType) => {
+							if (a.name.toLowerCase() < b.name.toLowerCase()) {
+								return 1;
+							}
+
+							if (a.name.toLowerCase() > b.name.toLowerCase()) {
+								return -1;
+							}
+
+							return 0;
+						}),
+				  ]; // need to fix
+		}
+		case "check": {
+			return state.filter((el) => el.age > 18); // need to fix
+		}
+		default:
+			return state;
+	}
+};
